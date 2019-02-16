9cab4fa5402a6efd5cdc5cd7a0f0ed85ea01cbac
Owen Tsai <owentsai.v@gmail.com>
2019-02-16 11:23:44 +0800

    [App] Add new components
    Add following components related to APP main view and layout:
    - AppToolbar,
    - AppSideMenu,
    - AppRightPanel

---
+++ envision-frontend/src/components/AppSideMenu.vue
@@ -0,0 +1,80 @@
+<template>
+
+  <v-navigation-drawer
+    fixed
+    clipped
+    class="grey lighten-4"
+    app
+    v-model="drawer"
+    style="z-index: 1"
+    :mini-variant="mini"
+  >
+
+    <v-toolbar flat class="transparent">
+      <v-list class="pa-0" dense>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-btn icon @click="mini = !mini">
+              <v-icon v-if="!mini">chevron_left</v-icon>
+              <v-icon v-if="mini">chevron_right</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+          <v-list-tile-content style="font-size:12px; color: grey">收回菜单</v-list-tile-content>
+        </v-list-tile>
+        <v-divider></v-divider>
+      </v-list>
+    </v-toolbar>
+
+    <v-list dense class="grey lighten-4">
+      <template v-for="(item, i) in items">
+        <v-layout row v-if="item.heading" align-center :key="i">
+          <v-flex xs6>
+            <v-subheader v-if="item.heading">
+              {{ item.heading }}
+            </v-subheader>
+          </v-flex>
+        </v-layout>
+        <v-divider dark v-else-if="item.divider" class="my-3" :key="i"></v-divider>
+        <v-list-tile :key="i" v-else @click="">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              <span class="grey--text text--darken-1">{{ item.text }}</span>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </template>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+    mini: false,
+    items: [
+      { heading: '视图' },
+      { icon: 'home', text: '首页' },
+      { icon: 'school', text: '学习' },
+      { icon: 'forum', text: '论坛' },
+      { icon: 'public', text: '探索' },
+      { icon: 'person', text: '个人中心' },
+      { icon: 'create', text: '创作' },
+      { icon: 'group', text: '群组' },
+      { icon: 'shopping_cart', text: '礼品店' },
+      { icon: 'assignment', text: '社团事务' },
+      { icon: 'build', text: '管理后台' },
+      { divider: true },
+      { heading: '自定义链接' },
+      { icon: 'add', text: '添加新的快速链接' },
+    ]
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ envision-frontend/src/components/AppToolbar.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-toolbar :color="userColorTheme" app absolute clipped-left>
+    <span class="title ml-3 mr-5">Envision&nbsp;<span class="slim-text">Web</span></span>
+    <v-spacer></v-spacer>
+  
+    <v-tooltip bottom>
+      <v-btn icon slot="activator">
+        <v-badge left overlap color=red>
+          <span slot="badge">!</span>
+            <v-icon>notifications</v-icon>
+        </v-badge>
+      </v-btn>
+      <span>通知</span>
+    </v-tooltip>
+
+    <v-menu>
+      <v-btn slot="activator" fab flat small>
+        <v-tooltip bottom>
+          <v-avatar color="grey lighten-4" :size=40 slot="activator">
+            <img :src="avatarHash" alt="avatar">
+          </v-avatar>
+          <span>我的</span>
+        </v-tooltip>
+      </v-btn>
+      <v-list dense>
+        <v-list-tile avatar class="my-2">
+          <v-list-tile-avatar>
+            <img :src="avatarHash" alt="avatar" :size=40>
+          </v-list-tile-avatar>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ username }}</v-list-tile-title>
+            <v-list-tile-sub-title>{{ userDescription }}</v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile
+          v-for="(item, index) in menuItems"
+          :key="index"
+          avatar
+          @click=""
+        >
+          <v-list-tile-avatar>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-avatar>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
+      
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    isUserLogged: true,
+    avatarHash: 'https://vuetifyjs.com/apple-touch-icon-180x180.png',
+    userColorTheme: 'amber',
+    username: 'Owen Tsai',
+    userDescription: '2015级自动化3班',
+    menuItems: [
+      { title: '个人中心', icon: 'person' },
+      { title: '我的草稿', icon: 'subject' },
+      { title: '关注内容', icon: 'stars' },
+      { title: '系统设置', icon: 'settings' },
+      { title: '退出登录', icon: 'exit_to_app' },
+    ]
+  })
+}
+</script>
+
+<style>
+.slim-text {
+  font-weight: 350;
+}
+</style>
