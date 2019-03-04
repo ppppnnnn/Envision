d24a37cbb494b2d92fd2c96b4390a3a46f7d46c2
know-cherish <485176679@qq.com>
2019-03-04 13:06:28 +0800

    后台管理

--- envision/settings.py
+++ envision/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'db_envision',
         'USER': 'root',
-        'PASSWORD': 'DWY5201314',
+        'PASSWORD': '*',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
